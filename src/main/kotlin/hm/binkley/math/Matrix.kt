78c36c496f3d17a0170d00479fb61faf5a5d53f0
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2021-01-12 08:50:28 -0600

    A working 2x2 matrix

--- src/main/kotlin/hm/binkley/math/Matrix.kt
+++ src/main/kotlin/hm/binkley/math/Matrix.kt
@@ -1,8 +1,53 @@
 package hm.binkley.math
 
-class Matrix {
-    fun doNothing(useless: Boolean) {
-        if (useless) return
-        else throw UnsupportedOperationException()
+import java.util.Objects.hash
+
+open class Matrix<T>(
+    val order: Int,
+    defaultValue: T,
+    private val values: MutableList<MutableList<T>> =
+        (ArrayList<MutableList<T>>(order)).apply {
+            repeat(order) {
+                add(
+                    ArrayList<T>(order).apply {
+                        repeat(order) {
+                            add(defaultValue)
+                        }
+                    }
+                )
+            }
+        },
+) {
+    init {
+        // TODO: Is there any math sense to an order-0 matrix?
+        if (1 > order) throw ArithmeticException("Bad matrix order: $order")
+    }
+
+    operator fun get(i: Int, j: Int) = values[i][j]
+    operator fun set(i: Int, j: Int, value: T) {
+        values[i][j] = value
+    }
+
+    override fun equals(other: Any?): Boolean = this === other ||
+        other is Matrix<*> &&
+        order == other.order &&
+        null == values.asSequence().filterIndexed { i, it ->
+        it != other.values[i]
+    }.firstOrNull()
+
+    override fun hashCode(): Int = hash(values)
+
+    override fun toString(): String = values.joinToString("\n ", "[", "]") {
+        it.joinToString(", ")
+    }
+}
+
+open class Matrix2x2<T>(
+    defaultValue: T,
+) : Matrix<T>(2, defaultValue)
+
+open class IntMatrix2x2 : Matrix2x2<Int>(0) {
+    companion object {
+        fun zero() = IntMatrix2x2()
     }
 }
--- src/test/kotlin/hm/binkley/math/MatrixTest.kt
+++ src/test/kotlin/hm/binkley/math/MatrixTest.kt
@@ -1,18 +1,70 @@
 package hm.binkley.math
 
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 internal class MatrixTest {
     @Test
-    fun `useless placeholder run`() {
-        Matrix().doNothing(true)
+    fun `should initialize with zeroes`() {
+        val m = IntMatrix2x2.zero()
+        for (i in 0 until 2)
+            for (j in 0 until 2)
+                m[i, j] shouldBe 0
+
+        IntMatrix2x2() shouldBe IntMatrix2x2.zero()
     }
 
     @Test
-    fun `also useless placeholder run`() {
-        assertThrows<UnsupportedOperationException> {
-            Matrix().doNothing(false)
+    fun `should complain for bad order`() {
+        shouldNotThrow<ArithmeticException> {
+            Matrix(order = 1, defaultValue = 0)
         }
+        shouldThrow<ArithmeticException> {
+            Matrix(order = 0, defaultValue = 0)
+        }
+    }
+
+    @Test
+    fun `should have correct order`() {
+        IntMatrix2x2().order shouldBe 2
+    }
+
+    @Test
+    fun `should equate`() {
+        (IntMatrix2x2() == IntMatrix2x2()).shouldBeTrue()
+        IntMatrix2x2().equals(3).shouldBeFalse()
+    }
+
+    @Test
+    fun `should hash`() {
+        IntMatrix2x2().hashCode() shouldNotBe 0
+
+        val m = IntMatrix2x2()
+        m[0, 0] = 3
+
+        m.hashCode() shouldBe m.hashCode()
+        m.hashCode() shouldNotBe IntMatrix2x2.zero().hashCode()
+    }
+
+    @Test
+    fun `should pretty print`() {
+        IntMatrix2x2.zero().toString() shouldBe """
+            [0, 0
+             0, 0]
+        """.trimIndent()
+    }
+
+    @Test
+    fun `should mutate`() {
+        val m = IntMatrix2x2()
+        m[0, 0] = 3
+
+        m[0, 0] shouldBe 3
+        m shouldNotBe IntMatrix2x2.zero()
     }
 }
